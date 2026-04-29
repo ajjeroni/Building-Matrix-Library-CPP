7ade49b253dc25e7866f59689d26174ad91cf8f7
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2026-04-29 04:16:02 +0000

    finished member variables

--- matrix.h
+++ matrix.h
@@ -3,12 +3,23 @@
 
 template <typename T>
 class Matrix {
+  /*
+  don't have to add the private access specifier
+  all things are private by default in a class
+  just doing it for consistancy purposes
+  */
  private:
   /*
   size_t is a data type that represents sizes and counts, unsigned
   (non-negative) guaranteed to be big enough to represent the size of any object
   in memory
   */
-  size_t cols;
-  size_t rows;
+  size_t cols_;
+  size_t rows_;
+
+  std::vector<Type> data_;
+  std::tuple<size_t, size_t> shape_;
+  int numel_ = rows_ * cols_;
+
+ public:
 };
